229b105aa8f9eaf2609a7af976c2cd06bd0a2f11
csteam <mplageman@pivotal.io>
2019-06-20 13:44:42 -0700

    add comment

--- src/backend/executor/nodeHashjoin.c
+++ src/backend/executor/nodeHashjoin.c
@@ -575,6 +575,8 @@ ExecHashJoinImpl(PlanState *pstate, bool parallel)
 						 */
 						if (node->first_outer_offset_match_status && HJ_FILL_OUTER(node))
 						{
+							// TODO: why is inlining this giving wrong results
+							// it should be semantically the same
 							node->hj_JoinState = HJ_ADAPTIVE_EMIT_UNMATCHED;
 							cursor = node->first_outer_offset_match_status;
 							node->first_outer_offset_match_status = NULL;
