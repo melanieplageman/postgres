fcb4a319f1ee6408e983998b1e492fe34755f8e7
Melanie Plageman <melanieplageman@gmail.com>
2019-10-03 08:58:02 -0700

    Refactor and remove unneeded new batch phase
    I've moved combining function to hashjoin since it concerns hashjoin
    I've also removed some unused code.
    
    Most importantly, this commit removes one of the phases I had added for
    needs new batch parallel state machine. Before I insisted all workers
    wait until combining was done and then close their files. After
    understanding the sharedfileset API better, I see this is not necessary

--- src/include/utils/sharedtuplestore.h
+++ src/include/utils/sharedtuplestore.h
@@ -61,31 +61,16 @@ extern void sts_end_parallel_scan(SharedTuplestoreAccessor *accessor);
 
 // TODO: this API is not great -- passing count_tuples flag. However, I don't want to count tuples for the inner side
 extern void sts_puttuple(SharedTuplestoreAccessor *accessor, void *meta_data, MinimalTuple tuple, bool count_tuples);
-
 extern uint32 sts_gettuplenum(SharedTuplestoreAccessor *accessor);
-
 extern int sts_get_my_participant_number(SharedTuplestoreAccessor *accessor);
 extern void sts_make_STA_outerMatchStatuses(SharedTuplestoreAccessor *accessor, int batchno, char *name);
 extern BufFile *sts_get_my_STA_outerMatchStatuses(SharedTuplestoreAccessor *accessor);
-
 extern MinimalTuple sts_parallel_scan_next(SharedTuplestoreAccessor *accessor, void *meta_data, bool is_outer);
-
-extern void sts_cleanup_STA_outer_match_status_files(SharedTuplestoreAccessor *accessor);
-
 extern void sts_bitmap_filename(char *name, SharedTuplestoreAccessor *accessor, int participant);
 extern SharedFileSet *sts_get_fileset(SharedTuplestoreAccessor *accessor);
+extern void populate_outer_match_statuses(SharedTuplestoreAccessor *accessor, BufFile *outer_match_statuses[], char **outer_match_status_filenames);
 
-extern void
-print_tuplenums(SharedTuplestoreAccessor *accessor, int batchno, BufFile *combined_bitmap_file,
-				BufFile **outer_read_files, int num_outer_read_files);
-extern void
-combine_outer_match_statuses(SharedTuplestoreAccessor *accessor, BufFile *outer_match_statuses[], int length, size_t num_bytes, int batchno, BufFile **combined_bitmap_file);
-extern void
-populate_outer_match_statuses(SharedTuplestoreAccessor *accessor, BufFile *outer_match_statuses[], char **outer_match_status_filenames);
-extern void
-close_outer_match_statuses(SharedTuplestoreAccessor *accessor, BufFile *outer_match_statuses[], int length);
+extern int sts_participants(SharedTuplestoreAccessor *accessor);
 
-extern
-void sts_rewind_all_outer_files(SharedTuplestoreAccessor *accessor, BufFile **outer_read_files, int length);
 
 #endif							/* SHAREDTUPLESTORE_H */
