7bd3d94077cfbfac23590750337209db41672cad
Melanie Plageman <melanieplageman@gmail.com>
2019-12-23 08:47:09 -0800

    passing

--- src/include/executor/adaptiveHashjoin.h
+++ src/include/executor/adaptiveHashjoin.h
@@ -4,5 +4,7 @@
 
 extern bool ExecParallelHashJoinNewChunk(HashJoinState *hjstate, bool advance_from_probing);
 extern bool ExecParallelHashJoinNewBatch(HashJoinState *hjstate);
+extern void combine_outer_match_statuses(BufFile *outer_match_statuses[], int length, size_t num_bytes,
+	int batchno, BufFile **combined_bitmap_file);
 
 #endif /* ADAPTIVE_HASHJOIN_H */
