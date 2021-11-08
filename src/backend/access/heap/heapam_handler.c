db443ad715fe4e6630f9bd386dcb3d962fe3e6a5
Melanie Plageman <melanieplageman@gmail.com>
2021-11-08 16:20:02 -0500

    more fix

--- src/backend/access/heap/heapam_handler.c
+++ src/backend/access/heap/heapam_handler.c
@@ -2140,11 +2140,9 @@ bitmapheap_pgsr_next_single(uintptr_t pgsr_private, PgAioInProgress *aio, uintpt
 		tbmres = palloc0(sizeof(TBMIterateResult) + MAX_TUPLES_PER_PAGE * sizeof(OffsetNumber));
 	else
 	{
-		ListCell *cell = llast(bhs_state->all_tbmres);
+		ListCell *cell = linitial(bhs_state->all_tbmres);
 		tbmres = (TBMIterateResult *) cell;
-		// TODO: list_remove_ptr doesn't exist in the list API -- it would
-		// essentially truncate the list to not include this member - without freeing any memory
-		bhs_state->all_tbmres = list_delete_cell(bhs_state->all_tbmres, cell);
+		bhs_state->all_tbmres = list_delete(bhs_state->all_tbmres, cell);
 	}
 
 	for (;;)
