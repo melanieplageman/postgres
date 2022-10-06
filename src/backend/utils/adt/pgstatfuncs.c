0d5fc7da60f6b02259b8dd1d2eab25967cb9a95a
Melanie Plageman <melanieplageman@gmail.com>
2022-10-06 12:23:38 -0400

    Aggregate IO operation stats per BackendType
    Stats on IOOps for all IOContexts for a backend are tracked locally. Add
    functionality for backends to flush these stats to shared memory and
    accumulate them with those from all other backends, exited and live.
    Also add reset and snapshot functions used by cumulative stats system
    for management of these statistics.
    
    The aggregated stats in shared memory could be extended in the future
    with per-backend stats -- useful for per connection IO statistics and
    monitoring.
    
    Some BackendTypes will not flush their pending statistics at regular
    intervals and explicitly call pgstat_flush_io_ops() during the course of
    normal operations to flush their backend-local IO operation statistics
    to shared memory in a timely manner.
    
    Because not all BackendType, IOOp, IOContext combinations are valid, the
    validity of the stats is checked before flushing pending stats and
    before reading in the existing stats file to shared memory.
    
    Author: Melanie Plageman <melanieplageman@gmail.com>
    Reviewed-by: Andres Freund <andres@anarazel.de>
    Reviewed-by: Justin Pryzby <pryzby@telsasoft.com>
    Reviewed-by: Kyotaro Horiguchi <horikyota.ntt@gmail.com>
    Reviewed-by: Maciek Sakrejda <m.sakrejda@gmail.com>
    Reviewed-by: Lukas Fittl <lukas@fittl.com>
    Discussion: https://www.postgresql.org/message-id/flat/20200124195226.lth52iydq2n2uilq%40alap3.anarazel.de

--- src/backend/utils/adt/pgstatfuncs.c
+++ src/backend/utils/adt/pgstatfuncs.c
@@ -2084,6 +2084,8 @@ pg_stat_reset_shared(PG_FUNCTION_ARGS)
 		pgstat_reset_of_kind(PGSTAT_KIND_BGWRITER);
 		pgstat_reset_of_kind(PGSTAT_KIND_CHECKPOINTER);
 	}
+	else if (strcmp(target, "io") == 0)
+		pgstat_reset_of_kind(PGSTAT_KIND_IOOPS);
 	else if (strcmp(target, "recovery_prefetch") == 0)
 		XLogPrefetchResetStats();
 	else if (strcmp(target, "wal") == 0)
@@ -2092,7 +2094,7 @@ pg_stat_reset_shared(PG_FUNCTION_ARGS)
 		ereport(ERROR,
 				(errcode(ERRCODE_INVALID_PARAMETER_VALUE),
 				 errmsg("unrecognized reset target: \"%s\"", target),
-				 errhint("Target must be \"archiver\", \"bgwriter\", \"recovery_prefetch\", or \"wal\".")));
+				 errhint("Target must be \"archiver\", \"io\", \"bgwriter\", \"recovery_prefetch\", or \"wal\".")));
 
 	PG_RETURN_VOID();
 }
