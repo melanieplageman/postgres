0e9633ebcb0195b7dbecda628da8efdaaa8f626f
Melanie Plageman <melanieplageman@gmail.com>
2023-04-03 11:22:18 -0400

    Separate vacuum cost variables from gucs
    Vacuum code run both by autovacuum workers and a backend doing
    VACUUM/ANALYZE previously used VacuumCostLimit and VacuumCostDelay which
    were the global variables for the gucs vacuum_cost_limit and
    vacuum_cost_delay. Autovacuum workers needed to override these variables
    with their own values, derived from autovacuum_vacuum_cost_limit and
    autovacuum_vacuum_cost_delay and worker cost limit balancing logic. This
    led to confusing code which, in some cases, both derived and set a new
    value of VacuumCostLimit from VacuumCostLimit.
    
    In preparation for refreshing these guc values more often, separate
    these variables from the gucs themselves and add a function to update
    the global variables using the gucs and existing logic.
    
    Per suggestion by Kyotaro Horiguchi
    
    Reviewed-by: Masahiko Sawada <sawada.mshk@gmail.com>
    Discussion: https://www.postgresql.org/message-id/flat/CAAKRu_ZngzqnEODc7LmS1NH04Kt6Y9huSjz5pp7%2BDXhrjDA0gw%40mail.gmail.com

--- src/backend/commands/vacuumparallel.c
+++ src/backend/commands/vacuumparallel.c
@@ -995,8 +995,9 @@ parallel_vacuum_main(dsm_segment *seg, shm_toc *toc)
 												 false);
 
 	/* Set cost-based vacuum delay */
-	VacuumCostActive = (VacuumCostDelay > 0);
 	VacuumCostBalance = 0;
+	VacuumUpdateCosts();
+	VacuumCostActive = (VacuumCostDelay > 0);
 	VacuumPageHit = 0;
 	VacuumPageMiss = 0;
 	VacuumPageDirty = 0;
