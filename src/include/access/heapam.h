9abc17497f49d3a88262a895994454aab1805c9f
Melanie Plageman <melanieplageman@gmail.com>
2024-01-07 17:03:17 -0500

    Inline heap_freeze_execute_prepared()
    In order to merge freeze and prune records, the execution of tuple
    freezing and the WAL logging of the changes to the page must be
    separated so that the WAL logging can be combined with prune WAL
    logging. This commit makes a helper for the tuple freezing and then
    inlines the contents of heap_freeze_execute_prepared() where it is
    called in heap_page_prune(). The original function,
    heap_freeze_execute_prepared() is retained because the "no prune" case
    in heap_page_prune() must still be able to emit a freeze record.

--- src/include/access/heapam.h
+++ src/include/access/heapam.h
@@ -14,6 +14,7 @@
 #ifndef HEAPAM_H
 #define HEAPAM_H
 
+#include "access/heapam_xlog.h"
 #include "access/relation.h"	/* for backward compatibility */
 #include "access/relscan.h"
 #include "access/sdir.h"
@@ -314,9 +315,16 @@ extern void heap_pre_freeze_checks(Buffer buffer,
 extern void heap_freeze_execute_prepared(Relation rel, Buffer buffer,
 										 TransactionId snapshotConflictHorizon,
 										 HeapTupleFreeze *tuples, int ntuples);
+
+extern void heap_freeze_prepared_tuples(Buffer buffer,
+										HeapTupleFreeze *tuples, int ntuples);
 extern bool heap_freeze_tuple(HeapTupleHeader tuple,
 							  TransactionId relfrozenxid, TransactionId relminmxid,
 							  TransactionId FreezeLimit, TransactionId MultiXactCutoff);
+
+extern int	heap_log_freeze_plan(HeapTupleFreeze *tuples, int ntuples,
+								 xl_heap_freeze_plan *plans_out,
+								 OffsetNumber *offsets_out);
 extern bool heap_tuple_should_freeze(HeapTupleHeader tuple,
 									 const struct VacuumCutoffs *cutoffs,
 									 TransactionId *NoFreezePageRelfrozenXid,
