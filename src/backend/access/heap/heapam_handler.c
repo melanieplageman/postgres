cf85e644c7adf5e111e64e72ba24c81ef102bc48
Melanie Plageman <melanieplageman@gmail.com>
2024-04-06 08:51:40 -0400

    table_scan_bitmap_next_block counts lossy and exact pages
    Future commits will remove the TBMIterateResult from BitmapHeapNext() --
    pushing it into the table AM-specific code. So the table AM must keep
    track of whether or not individual TBMIterateResult's blocks were
    represented lossily in the bitmap for the purposes of EXPLAIN counters.
    
    Author: Melanie Plageman
    Reviewed-by: Tomas Vondra
    Discussion: https://postgr.es/m/CAAKRu_ZwCwWFeL_H3ia26bP2e7HiKLWt0ZmGXPVwPO6uXq0vaA%40mail.gmail.com

--- src/backend/access/heap/heapam_handler.c
+++ src/backend/access/heap/heapam_handler.c
@@ -2188,7 +2188,8 @@ heapam_estimate_rel_size(Relation rel, int32 *attr_widths,
 
 static bool
 heapam_scan_bitmap_next_block(TableScanDesc scan,
-							  TBMIterateResult *tbmres)
+							  TBMIterateResult *tbmres,
+							  long *lossy_pages, long *exact_pages)
 {
 	HeapScanDesc hscan = (HeapScanDesc) scan;
 	BlockNumber block = tbmres->blockno;
@@ -2315,6 +2316,11 @@ heapam_scan_bitmap_next_block(TableScanDesc scan,
 
 	Assert(ntup <= MaxHeapTuplesPerPage);
 	hscan->rs_ntuples = ntup;
+
+	if (tbmres->ntuples < 0)
+		(*lossy_pages)++;
+	else
+		(*exact_pages)++;
 
 	return ntup > 0;
 }
