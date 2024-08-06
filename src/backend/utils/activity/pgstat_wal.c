56a1a10b3367ea7c0d9fb8728bd72c82e4296dfa
Melanie Plageman <melanieplageman@gmail.com>
2024-08-05 20:33:15 -0400

    Add global LSNTimeStream to PgStat_WalStats
    Add and maintain a global instance of an LSNTimeStream to
    PgStat_WalStats. It is statistical information on WAL generation rate,
    so it makes sense as part of the WAL stats data structure.
    It will be used to drive adaptive freezing behavior.
    
    Background writer is tasked with inserting new LSN, time pairs to the
    global stream in its main loop at regular intervals. There is precedent
    for background writer performing such tasks: bgwriter already
    periodically logs snapshots into the WAL for the benefit of standbys.

--- src/backend/utils/activity/pgstat_wal.c
+++ src/backend/utils/activity/pgstat_wal.c
@@ -39,7 +39,6 @@ static WalUsage prevWalUsage;
  */
 static uint32 lsntime_to_drop(LSNTimeStream *stream);
 
-pg_attribute_unused()
 static void lsntime_insert(LSNTimeStream *stream, TimestampTz time,
 						   XLogRecPtr lsn);
 
@@ -297,6 +296,21 @@ lsntime_insert(LSNTimeStream *stream, TimestampTz time,
 				sizeof(LSNTime) * (stream->length - 1 - drop));
 
 	stream->data[stream->length - 1] = entrant;
+}
+
+
+/*
+ * Utility function for inserting a new member into the LSNTimeStream member
+ * of WAL stats.
+ */
+void
+pgstat_wal_update_lsntime_stream(XLogRecPtr lsn, TimestampTz time)
+{
+	PgStatShared_Wal *stats_shmem = &pgStatLocal.shmem->wal;
+
+	LWLockAcquire(&stats_shmem->lock, LW_EXCLUSIVE);
+	lsntime_insert(&stats_shmem->stats.stream, time, lsn);
+	LWLockRelease(&stats_shmem->lock);
 }
 
 
