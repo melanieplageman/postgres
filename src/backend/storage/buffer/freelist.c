0bdbfadb4eae17ba2dd5d03e62362c8670898a0f
Melanie Plageman <melanieplageman@gmail.com>
2026-01-07 14:56:49 -0500

    Eagerly flush bulkwrite strategy ring
    Operations using BAS_BULKWRITE (COPY FROM and createdb) will inevitably
    need to flush buffers in the strategy ring in order to reuse them. By
    eagerly flushing the buffers in a larger run, we encourage larger writes
    at the kernel level and less interleaving of WAL flushes and data file
    writes. The effect is mainly noticeable with multiple parallel COPY
    FROMs. In this case, client backends achieve higher write throughput and
    end up spending less time waiting on acquiring the lock to flush WAL.
    Larger flush operations also mean less time waiting for flush operations
    at the kernel level.
    
    The heuristic for eager eviction is to only flush buffers in the
    strategy ring which do not require a WAL flush.
    
    This patch also is a step toward AIO writes, as it lines up multiple
    buffers that can be issued asynchronously once the infrastructure
    exists.
    
    Author: Melanie Plageman <melanieplageman@gmail.com>
    Reviewed-by: Chao Li <li.evan.chao@gmail.com>
    Reviewed-by: Nazir Bilal Yavuz <byavuz81@gmail.com>
    Earlier version Reviewed-by: Kirill Reshke <reshkekirill@gmail.com>
    Discussion: https://postgr.es/m/2FA0BAC7-5413-4ABD-94CA-4398FE77750D%40gmail.com
    Discussion: https://postgr.es/m/flat/CAAKRu_Yjn4mvN9NBxtmsCQSGwup45CoA4e05nhR7ADP-v0WCig%40mail.gmail.com

--- src/backend/storage/buffer/freelist.c
+++ src/backend/storage/buffer/freelist.c
@@ -748,6 +748,52 @@ next_buffer:;
 	}
 }
 
+/*
+ * Returns the next buffer in the ring after the one at cursor and increments
+ * cursor.
+ */
+Buffer
+StrategyNextBuffer(BufferAccessStrategy strategy, int *cursor)
+{
+	if (++(*cursor) >= strategy->nbuffers)
+		*cursor = 0;
+
+	return strategy->buffers[*cursor];
+}
+
+/*
+ * Return the current slot in the strategy ring.
+ */
+int
+StrategyGetCurrentIndex(BufferAccessStrategy strategy)
+{
+	return strategy->current;
+}
+
+/*
+ * Some BufferAccessStrategies support eager flushing -- which is flushing
+ * buffers in the ring before they are needed. This can lead to better I/O
+ * patterns than lazily flushing buffers immediately before reusing them.
+ */
+bool
+StrategySupportsEagerFlush(BufferAccessStrategy strategy)
+{
+	Assert(strategy);
+
+	switch (strategy->btype)
+	{
+		case BAS_BULKWRITE:
+			return true;
+		case BAS_VACUUM:
+		case BAS_NORMAL:
+		case BAS_BULKREAD:
+			return false;
+		default:
+			elog(ERROR, "unrecognized buffer access strategy: %d",
+				 (int) strategy->btype);
+			return false;
+	}
+}
 
 
 /*
